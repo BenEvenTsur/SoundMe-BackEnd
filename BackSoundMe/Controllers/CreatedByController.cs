446c4b66214b5ae4489a7c60996044d4d3e12203
Beneventsur <beneventsur@gmail.com>
2018-10-04 19:14:26 +0300

    update for http post

---
+++ BackSoundMe/Controllers/CreatedByController.cs
@@ -0,0 +1,74 @@
+﻿using BackSoundMe.Abstracts;
+using BackSoundMe.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BackSoundMe.Controllers
+{
+   // [RoutePrefix("createdby")]
+    public class CreatedByController : ApiController
+    {
+       /* [HttpGet]
+        [Route("getpublicsongs")]
+        public IHttpActionResult GetPublicSongs()
+        {
+            try
+            {
+                SongDal songsDAl = new SongDal();
+                List<Song> songs = songsDAl.GetPubicSongs().ToList();
+
+                return Ok(songs);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+            [HttpGet]
+            [Route("getsongbyid/{key}")]
+            public IHttpActionResult GetSongById(int key)
+            {
+                try
+                {
+                    if (key < 0)
+                        throw new ArgumentException("Key as Guid is Empty.");
+
+                    IDal<Song, int> songsDAl = new SongDal();
+                    Song song = songsDAl.GetByID(key);
+
+                    return Ok(song);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+
+
+
+            [HttpGet]
+            [Route("getsongsbyartist/{key}")]
+            public IHttpActionResult getSongsByArtist(int key)
+            {
+                try
+                {
+                    IDal<Song, int> songsDAl = new SongDal();
+                    List<Song> songs = songsDAl.GetAll().ToList();
+
+                    return Ok(songs);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+*/
+         
+        
+    }
+}
